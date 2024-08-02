4859fe1b5b58ac9cff200ab720ecca04bfeab762
himanshu864 <himanshu96864@gmail.com>
2024-08-02 16:19:45 +0530

    minor: formatting prettier

--- script/easyAI.js
+++ script/easyAI.js
@@ -2,13 +2,11 @@ const tiles = document.querySelectorAll(".tile");
 
 // Computer Clicks
 export const algoPlayerTwo = (grid) => {
-    // Random
-    let ok = [];
-    for (let i = 0; i < 3; i++)
-        for (let j = 0; j < 3; j++)
-            if (grid[i][j] == -1)
-                ok.push(3 * i + j);
+  // Random
+  let ok = [];
+  for (let i = 0; i < 3; i++)
+    for (let j = 0; j < 3; j++) if (grid[i][j] == -1) ok.push(3 * i + j);
 
-    let r = Math.floor(Math.random() * ok.length)
-    tiles[ok[r]].click();
-}
+  let r = Math.floor(Math.random() * ok.length);
+  tiles[ok[r]].click();
+};
--- script/main.js
+++ script/main.js
@@ -17,7 +17,7 @@ const easyButton = document.getElementById("easy");
 const mediumButton = document.getElementById("mid");
 const impossibleButton = document.getElementById("imp");
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let areYouSingle = true;
 let gameOver = false;
@@ -26,161 +26,157 @@ let difficulty = 2;
 let p1 = 0;
 let p2 = 0;
 let grid = [
-    [-1, -1, -1],
-    [-1, -1, -1],
-    [-1, -1, -1]
+  [-1, -1, -1],
+  [-1, -1, -1],
+  [-1, -1, -1],
 ];
 
 // Choose Single or Multi-Player
 sp.addEventListener("click", () => {
-    areYouSingle = true;
-    mode.classList.add("hide");
-    difficultySlider.classList.remove("hide");
-})
+  areYouSingle = true;
+  mode.classList.add("hide");
+  difficultySlider.classList.remove("hide");
+});
 mp.addEventListener("click", () => {
-    areYouSingle = false;
-    mode.classList.add("hide");
-    button.classList.remove("hide");
-})
+  areYouSingle = false;
+  mode.classList.add("hide");
+  button.classList.remove("hide");
+});
 
 // Choose Difficulity
 easyButton.addEventListener("click", () => {
-    difficulty = 1;
-    difficultySlider.classList.add("hide");
-    button.classList.remove("hide");
-})
+  difficulty = 1;
+  difficultySlider.classList.add("hide");
+  button.classList.remove("hide");
+});
 mediumButton.addEventListener("click", () => {
-    difficulty = 2;
-    difficultySlider.classList.add("hide");
-    button.classList.remove("hide");
-})
+  difficulty = 2;
+  difficultySlider.classList.add("hide");
+  button.classList.remove("hide");
+});
 impossibleButton.addEventListener("click", () => {
-    difficulty = 3;
-    difficultySlider.classList.add("hide");
-    button.classList.remove("hide");
-})
+  difficulty = 3;
+  difficultySlider.classList.add("hide");
+  button.classList.remove("hide");
+});
 
 // Adds event listens to empty tiles
 const startGame = () => {
-    tiles.forEach(tile => {
-        if (tile.innerText == "")
-            tile.addEventListener("click", handleClick, { once: true });
-    })
-}
+  tiles.forEach((tile) => {
+    if (tile.innerText == "")
+      tile.addEventListener("click", handleClick, { once: true });
+  });
+};
 
 const stopGame = () => {
-    tiles.forEach(tile => {
-        if (tile.innerText == "") {
-            tile.removeEventListener("click", handleClick);
-        }
-    });
-}
+  tiles.forEach((tile) => {
+    if (tile.innerText == "") {
+      tile.removeEventListener("click", handleClick);
+    }
+  });
+};
 
 // Start game by pressing button
 const pressButtonRestart = () => {
-    button.addEventListener("click", () => {
-        button.classList.add("hide");
-        score.classList.add("hide");
-        container.classList.remove("hide");
+  button.addEventListener("click", () => {
+    button.classList.add("hide");
+    score.classList.add("hide");
+    container.classList.remove("hide");
 
-        // Reset everything on click
-        grid = [
-            [-1, -1, -1],
-            [-1, -1, -1],
-            [-1, -1, -1]
-        ];
-        tiles.forEach(tile => {
-            tile.innerText = "";
-            tile.style.backgroundColor = "lavender";
-            tile.classList.remove("focusOut");
-            tile.classList.remove("blink");
-        });
-        playerOneTurn = true;
-        gameOver = false;
+    // Reset everything on click
+    grid = [
+      [-1, -1, -1],
+      [-1, -1, -1],
+      [-1, -1, -1],
+    ];
+    tiles.forEach((tile) => {
+      tile.innerText = "";
+      tile.style.backgroundColor = "lavender";
+      tile.classList.remove("focusOut");
+      tile.classList.remove("blink");
+    });
+    playerOneTurn = true;
+    gameOver = false;
 
-        startGame();
-    })
-}
+    startGame();
+  });
+};
 
 // Restart Logic
 const restartButton = () => {
-    button.innerText = "Play Again";
-    button.classList.remove("hide");
-    score.classList.remove("hide");
+  button.innerText = "Play Again";
+  button.classList.remove("hide");
+  score.classList.remove("hide");
 
-    pressButtonRestart();
-}
+  pressButtonRestart();
+};
 
 // Modified grid based on player
 const handleClick = async (e) => {
-    const cell = e.target;
-    const idx = Array.from(tiles).indexOf(cell);
-    const row = Math.floor(idx / 3);
-    const col = idx % 3;
+  const cell = e.target;
+  const idx = Array.from(tiles).indexOf(cell);
+  const row = Math.floor(idx / 3);
+  const col = idx % 3;
 
-    if (playerOneTurn) {
-        cell.innerText = "X";
-        grid[row][col] = 1;
-    }
-    else {
-        cell.innerText = "O";
-        grid[row][col] = 0;
-    }
+  if (playerOneTurn) {
+    cell.innerText = "X";
+    grid[row][col] = 1;
+  } else {
+    cell.innerText = "O";
+    grid[row][col] = 0;
+  }
 
-    cell.style.backgroundColor = "blueviolet";
-    playerOneTurn = !playerOneTurn;
+  cell.style.backgroundColor = "blueviolet";
+  playerOneTurn = !playerOneTurn;
 
-    // Check if curr player wins
-    areYouWinningSon();
+  // Check if curr player wins
+  areYouWinningSon();
 
-    // Computer turn with delay
-    if (areYouSingle && !gameOver && !playerOneTurn) {
-        stopGame();
-        await delay(500);
-        startGame();
+  // Computer turn with delay
+  if (areYouSingle && !gameOver && !playerOneTurn) {
+    stopGame();
+    await delay(500);
+    startGame();
 
-        if (difficulty == 1)
-            easy(grid);
-        else if (difficulty == 2)
-            medium(grid);
-        else
-            impossible(grid);
-    }
-}
+    if (difficulty == 1) easy(grid);
+    else if (difficulty == 2) medium(grid);
+    else impossible(grid);
+  }
+};
 
 // Logic for gameover
 const areYouWinningSon = () => {
-    const result = scoreMate(grid);
+  const result = scoreMate(grid);
 
-    if (result != -1) {
-        if (result == 1) {
-            if (areYouSingle) {
-                score.innerText = "You Win!";
-            } else {
-                score.innerText = "You win, Player One!";
-            }
-            p1++;
-            scoreP1.innerText = p1;
-        }
-        if (result == 0) {
-            if (areYouSingle) {
-                score.innerText = "You Lose!";
-            } else {
-                score.innerText = "You win, Player Two!";
-            }
-            p2++;
-            scoreP2.innerText = p2;
-        }
-        if (result == 2) {
-            score.innerText = "It's a draw!";
-        }
-
-        gameOver = true;
-        // removes tiles click event onces game over and adds button
-        restartButton();
-        stopGame();
+  if (result != -1) {
+    if (result == 1) {
+      if (areYouSingle) {
+        score.innerText = "You Win!";
+      } else {
+        score.innerText = "You win, Player One!";
+      }
+      p1++;
+      scoreP1.innerText = p1;
     }
-}
+    if (result == 0) {
+      if (areYouSingle) {
+        score.innerText = "You Lose!";
+      } else {
+        score.innerText = "You win, Player Two!";
+      }
+      p2++;
+      scoreP2.innerText = p2;
+    }
+    if (result == 2) {
+      score.innerText = "It's a draw!";
+    }
+
+    gameOver = true;
+    // removes tiles click event onces game over and adds button
+    restartButton();
+    stopGame();
+  }
+};
 
 // START GAME
 pressButtonRestart();
